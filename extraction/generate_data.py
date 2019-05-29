46a51cbcf80983e842b5d53be609ae455c9c1d9a
thoum <indiofish@gmail.com>
2019-05-29 14:58:23 +0900

    add full file name to data

--- extraction/generate_data.py
+++ extraction/generate_data.py
@@ -9,11 +9,11 @@ FILE_PATH = "../tensorflow-hangul-recognition/image-data/hangul-images/"
 images = []
 for font in FONTS:
     # name[-6] is the position of the hangeul character
-    images += [(cv2.imread(name), font, name[-6]) for name in glob.glob(FILE_PATH + "*{}_*.jpeg".format(font))]
+    images += [(cv2.imread(name), name, font, name[-6]) for name in glob.glob(FILE_PATH + "*{}_*.jpeg".format(font))]
 
 data = []
-for (img, font, c) in images:
+for (img, name, font, c) in images:
     profile = get_profile(img)
-    data.append((np.array(profile), font, c))
+    data.append((np.array(profile), name, font, c))
 
 np.save("dataset", data)
