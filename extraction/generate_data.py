f8e25cb2c93aa382fc91501e895d325f39f5a09c
thoum <indiofish@gmail.com>
2019-05-29 14:46:54 +0900

    generate data from font images

---
+++ extraction/generate_data.py
@@ -0,0 +1,19 @@
+from features import get_profile
+import numpy as np
+import cv2
+import glob
+
+FONTS = ["NanumGothic", "JosunIlboMJ", "BareunBatangL", "10X10"]
+FILE_PATH = "../tensorflow-hangul-recognition/image-data/hangul-images/"
+
+images = []
+for font in FONTS:
+    # name[-6] is the position of the hangeul character
+    images += [(cv2.imread(name), font, name[-6]) for name in glob.glob(FILE_PATH + "*{}_*.jpeg".format(font))]
+
+data = []
+for (img, font, c) in images:
+    profile = get_profile(img)
+    data.append((np.array(profile), font, c))
+
+np.save("dataset", data)
