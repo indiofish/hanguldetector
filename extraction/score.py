4186bedc566dfb99093717facd04a923f126cf1d
thoum <indiofish@gmail.com>
2019-05-29 19:43:57 +0900

    fix

--- extraction/score.py
+++ extraction/score.py
@@ -5,8 +5,8 @@ def loss(ft1, ft2):
     if len(ft1) < len(ft2):
         #penalize those with different number of boxes?
         ft1 = np.pad(ft1, (0, len(ft2)-len(ft1)), 'constant',
-                constant_values=3)
+                constant_values=1)
     elif len(ft1) > len(ft2):
         ft2 = np.pad(ft2, (0, len(ft1)-len(ft2)), 'constant',
-                constant_values=3)
-    return np.linalg.norm(ft - testft)
+                constant_values=1)
+    return np.linalg.norm(ft1 - ft2)
