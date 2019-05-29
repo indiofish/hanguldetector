9aecd5046e4730bfb2f087936d5983141f4dc31e
thoum <indiofish@gmail.com>
2019-05-29 14:57:41 +0900

    move testing code to different file

---
+++ extraction/main.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import features
+import score
+
+testimgpath = "./testset/testga.png"
+img = cv2.imread(testimgpath)
+img_ft = features.get_profile(img)
+
+min_loss = 99999999 # some magic big number
+min_name = ''
+# cv2.destroyAllWindows()
+
+with load('./dataset.npy') as data:
+    for (feature, fullname, font, c) in data:
+        loss = score.loss(feature, img_ft)
+        if loss < min_loss:
+            min_loss = loss
+            min_name = fullname
+
+cv2.imshow("data", cv2.imread(min_name))
+cv2.imshow("test", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
