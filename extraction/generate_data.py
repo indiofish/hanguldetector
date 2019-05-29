e5e232e8515bb602e339c7bb1276b6d9fc2e8331
thoum <indiofish@gmail.com>
2019-05-29 15:27:53 +0900

    add multiprocessing for faster data generation

--- extraction/generate_data.py
+++ extraction/generate_data.py
@@ -2,18 +2,31 @@ from features import get_profile
 import numpy as np
 import cv2
 import glob
+from multiprocessing import Pool
 
-FONTS = ["NanumGothic", "JosunIlboMJ", "BareunBatangL", "10X10"]
+FONTS = ["nanumgodigL", "", "NanumGothic", "JosunIlboMJ", "BareunBatangL",
+"BareunBatangB", "10X10"]
 FILE_PATH = "../tensorflow-hangul-recognition/image-data/hangul-images/"
 
-images = []
-for font in FONTS:
-    # name[-6] is the position of the hangeul character
-    images += [(cv2.imread(name), name, font, name[-6]) for name in glob.glob(FILE_PATH + "*{}_*.jpeg".format(font))]
+THREADS = 4
 
-data = []
-for (img, name, font, c) in images:
-    profile = get_profile(img)
-    data.append((np.array(profile), name, font, c))
+def aux(entry):
+    """aux function for pooling"""
 
-np.save("dataset", data)
+    img, name, font, c = entry
+    return (get_profile(img), name, font, c)
+
+
+
+if __name__ == '__main__':
+
+    images = []
+    for font in FONTS:
+        # name[-6] is the position of the hangeul character
+        images += [(cv2.imread(name), name, font, name[-6]) for name in glob.glob(FILE_PATH + "*{}_*.jpeg".format(font))]
+
+    p = Pool(THREADS)
+
+    data = p.map(aux, images)
+
+    np.save("dataset", data)
