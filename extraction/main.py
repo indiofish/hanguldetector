43c402d13f194c9f3b311827f0064d5dcc814d07
SeijunChung <43940934+SeijunChung@users.noreply.github.com>
2019-05-30 16:21:11 +0900

    Add files via upload
    src for implementing HoG Descriptor
    - main
    - HoG
    - Visualize_data

--- extraction/main.py
+++ extraction/main.py
@@ -1,24 +1,21 @@
-import cv2
 import numpy as np
-import features
-import score
+from HoG import calculate_hog_features
+from visualize_data import *
+import timeit
 
-testimgpath = "./testset/testga.png"
-img = cv2.imread(testimgpath)
-img_ft = features.get_profile(img)
 
-min_loss = 99999999 # some magic big number
-min_name = ''
-# cv2.destroyAllWindows()
+if __name__ == '__main__':
 
-with load('./dataset.npy') as data:
-    for (feature, fullname, font, c) in data:
-        loss = score.loss(feature, img_ft)
-        if loss < min_loss:
-            min_loss = loss
-            min_name = fullname
+    # img = plt.imread("./data/break.png")
+    img = plt.imread("./data/an3.png")
+    # img = plt.imread("lena.jpg")
 
-cv2.imshow("data", cv2.imread(min_name))
-cv2.imshow("test", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    hist_bins = np.array([0, 20, 40, 60, 80, 100, 120, 140, 160])
+
+    start = timeit.default_timer()
+    hist_cells, hog_feature = calculate_hog_features(img, hist_bins)
+    print(timeit.default_timer() - start)
+
+    # visualize_histogram_all_cells(hist_cells, hist_bins)
+
+    visualize_image(hog_feature)
