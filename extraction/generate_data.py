75bc362e4656df06096134b14951dd547a136294
thoum <indiofish@gmail.com>
2019-06-01 16:58:39 +0900

    cleanup

--- extraction/generate_data.py
+++ extraction/generate_data.py
@@ -4,9 +4,8 @@ import cv2
 import glob
 from multiprocessing import Pool
 
-FONTS = [""]
-# TESTFONT = "SeoulHangangL"
-TESTFONT = "NanumPen"
+FONTS = [""] # if "", include every font
+TESTFONT = "" # if "", don't exclude the font 
 FILE_PATH = "../tensorflow-hangul-recognition/image-data/hangul-images/"
 
 THREADS = 4
@@ -18,17 +17,16 @@ def aux(entry):
     return (get_profile(img), name, font, c)
 
 
-
 if __name__ == '__main__':
     images = []
     for font in FONTS:
         # name[-6] is the position of the hangeul character
         for name in glob.glob(FILE_PATH+"*{}_*.jpeg".format(font)):
+            # exclude the testfont
             if TESTFONT not in name:
                 images += [(cv2.imread(name), name, font, name[-6])]
 
     p = Pool(THREADS)
 
     data = p.map(aux, images)
-
     np.save("dataset", data)
--- extraction/main.py
+++ extraction/main.py
@@ -42,4 +42,3 @@ if __name__ == '__main__':
     cv2.imshow("test", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
