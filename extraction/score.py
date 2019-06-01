028cbbf41221d72df10ee6e230fa6875e8f5e69a
thoum <indiofish@gmail.com>
2019-06-01 15:15:04 +0900

    instead of padding vectors of diffrent lenght for distance computation, penalize them heavily by a constant distance

--- extraction/score.py
+++ extraction/score.py
@@ -2,13 +2,6 @@ import numpy as np
 
 def loss(ft1, ft2):
     """return the euclidian distance between the two feature vectors"""
-    # if len(ft1) < len(ft2):
-        #penalize those with different number of boxes?
-        # ft1 = np.pad(ft1, (0, len(ft2)-len(ft1)), 'constant',
-                # constant_values=1)
-    # elif len(ft1) > len(ft2):
-        # ft2 = np.pad(ft2, (0, len(ft1)-len(ft2)), 'constant',
-                # constant_values=1)
     if len(ft1) == len(ft2):
         return np.linalg.norm(ft1 - ft2)
     else:
