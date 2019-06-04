2abe2b010efda20d7748f89da8cba86ae8f4442e
Hohoro15 <hsrabbit.95@gmail.com>
2019-06-04 18:42:09 +0900

    on way to edit

--- tensorflow-hangul-recognition/tools/boxhangul.py
+++ tensorflow-hangul-recognition/tools/boxhangul.py
@@ -1,13 +1,15 @@
 from outertest import *
 
-import argparse
-import io
-import os
+#import argparse
+#import io
+#import os
 import cv2
 import glob
 from PIL import Image
-import numpy as np
+import imageio
+#import numpy as np
 
+'''
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Default data paths.
@@ -19,19 +21,39 @@ DEFAULT_WIDTH = 64
 DEFAULT_HEIGHT = 64
 
 def processdata(label_file, data_dir):
+'''
+images = [(cv2.imread(im),im) for im in glob.glob("../image-data/data/*.jpeg")]
 
-    images = [(cv2.imread(im),im) for im in glob.glob("../image-data/data/*.jpeg")]
+#for (img,name) in images:
+img = images[0][0]
+x = get_feature2(img)
+left = x[0]
+top = x[1]
+right = x[2]
+bottom = x[3]
 
-    for (img,name) in images:
-        ft = get_feature2(img)
-        print(ft.shape)
+print(left)
+print(top)
+print(bottom)
+print(right)
+
+#cv2.rectangle(img,(left,top),(right,bottom),(0,0,255))
+cv2.imshow("draw",img)
+cv2.waitKey(0)
+
+    #print(ft.shape)
+    #print(ft)
+    #im = Image.fromarray(ft)
+    #im.save(name)
+    #imageio.imwrite(name,ft)
+        
 
 
 
 
 
 
-
+'''
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -43,3 +65,4 @@ if __name__ == '__main__':
                         help='Directory of database to modify.')
     args = parser.parse_args()
     processdata(args.label_file, args.data_dir)
+'''
